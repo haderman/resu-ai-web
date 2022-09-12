155928a4de557cb1dab16f78f660ccb4beb6a198
haderman <hadermania@gmail.com>
2022-09-12 20:57:08 +0300

    allow to align-items

--- src/components/cv/common/inline.tsx
+++ src/components/cv/common/inline.tsx
@@ -5,10 +5,12 @@ import { Size } from '../types';
 
 export type InlineProps = React.PropsWithChildren<BoxProps & {
   gap?: Size;
+  alignItems?: 'start' | 'center' | 'end';
 }>;
 
 export const Inline = styled(Box)<InlineProps>`
   display: inline-flex;
   flex-direction: row;
+  align-items: ${({ alignItems }) => alignItems || 'unset'};
   gap: ${props => props.theme.gap[props.gap ?? 'default']};
 `;
