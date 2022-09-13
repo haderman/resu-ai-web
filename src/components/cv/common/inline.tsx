29223aaf76787502a9a4d4c9f39c5ec325394347
haderman <hadermania@gmail.com>
2022-09-13 17:04:20 +0300

    add childrenWithSameWidth property

--- src/components/cv/common/inline.tsx
+++ src/components/cv/common/inline.tsx
@@ -4,8 +4,9 @@ import { Box, BoxProps } from './box';
 import { Size } from '../types';
 
 export type InlineProps = React.PropsWithChildren<BoxProps & {
-  gap?: Size;
-  alignItems?: 'start' | 'center' | 'end';
+  gap?: Size
+  alignItems?: 'start' | 'center' | 'end'
+  childrenWithSameWidth?: boolean
 }>;
 
 export const Inline = styled(Box)<InlineProps>`
@@ -13,4 +14,10 @@ export const Inline = styled(Box)<InlineProps>`
   flex-direction: row;
   align-items: ${({ alignItems }) => alignItems || 'unset'};
   gap: ${props => props.theme.gap[props.gap ?? 'default']};
+
+  > * {
+    ${({ childrenWithSameWidth }) => childrenWithSameWidth && `
+      flex: 1;
+    `}
+  }
 `;
