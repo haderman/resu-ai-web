e20e2eeae1bc305cbedd14a4bdd08efd155322c9
haderman <hadermania@gmail.com>
2022-09-14 12:36:31 +0300

    add fitContent prop to box
    the default behaviour for a box is to have a full width, I think this is very useful using grid layout but I wanted to make it optional adding this property

--- src/components/cv/common/box.tsx
+++ src/components/cv/common/box.tsx
@@ -6,9 +6,11 @@ export type BoxProps = React.PropsWithChildren<{
   padding?: Size
   background?: Color
   borderRadius?: Size
+  fitContent?: boolean
 }>
 
 export const Box = styled.div<BoxProps>`
+  width: ${props => props.fitContent ? 'fit-content' : '100%'};
   padding: ${props => props.theme.padding[props.padding ?? 'default']};
   background: ${props => props.theme.bg[props.background ?? 'default']};
   border-radius: ${props => props.theme.borderRadius[props.borderRadius ?? 'default']};
