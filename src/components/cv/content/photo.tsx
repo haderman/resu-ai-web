8dfed26a322a0c332eaaea7a783b9bd407a1bcf4
haderman <hadermania@gmail.com>
2022-09-25 15:13:17 +0300

    refactor Photo component

--- src/components/cv/content/photo.tsx
+++ src/components/cv/content/photo.tsx
@@ -1,16 +1,27 @@
 import Image from 'next/image';
 import styled from 'styled-components';
 
-export function Photo() {
+import { Color } from '../types';
+
+export type PhotoProps = {
+  background?: Color
+}
+
+export function Photo(props: PhotoProps) {
   return (
-    <Picture>
+    <Picture {...props}>
       <Image src="/profile-photo.png" alt="CV photo" layout="fill" objectFit="contain" />
     </Picture>
   );
 }
 
-const Picture = styled.picture`
+const Picture = styled.picture<PhotoProps>`
+  display: flex;
   width: 100%;
   height: 100%;
+
+  ${(props) => props.background && `
+    background-color: ${props.theme.colors[props.background].background};
+  `}
 `;
 
