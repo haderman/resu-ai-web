1fe13581f79ff6c4eb2e3cdeedb0ae140025831d
haderman <hadermania@gmail.com>
2022-10-03 19:39:17 +0300

    remove commented code and unused prop

--- src/components/cv/cv.tsx
+++ src/components/cv/cv.tsx
@@ -1,23 +1,5 @@
 import { PageLayout } from './page-layout';
 
 export function CvPrintablePage() {
-  return (
-    <PageLayout theme="dark-space" />
-      // photo="https://avatars.githubusercontent.com/u/10214025?v=4"
-      // description="I'm a software engineer with a passion for learning and teaching. I'm currently working at @microsoft as a software engineer on the Azure SDK team."
-      // strongSkills={['JavaScript', 'TypeScript', 'React', 'Node.js']}
-      // skills={['C#', 'Python', 'Java', 'Go', 'Docker', 'Kubernetes']}
-      // twitter="https://twitter.com/andrew_codes"
-      // github="haderman7"
-      // email="cardona.hader@gmail.com"
-      // experience={[
-      //   {
-      //     title: 'Software Engineer',
-      //     company: 'Microsoft',
-      //     description: 'Working on the Azure SDK team',
-      //     startDate: '2021-01-01',
-      //     endDate: '2021-01-01',
-      //   }
-      // ]}
-  );
+  return <PageLayout />;
 }
