13080c567e0f6fdf383accac9123d468827a0fc5
haderman <hadermania@gmail.com>
2022-10-30 12:37:30 +0300

    create tests for Resume type serialization

--- __tests__/shared/types/resume/resume.test.ts
+++ __tests__/shared/types/resume/resume.test.ts
@@ -1,9 +1,24 @@
-import { expect, jest, test } from '@jest/globals';
+import { expect } from '@jest/globals';
+import Fuzz from 'jest-fuzz';
 
 import { Resume } from '@/shared/types/resume';
 
-describe('Resume', () => {
-  test('should be defined', () => {
-    expect(Resume.create).toBeDefined();
+var contentFuzzer = Fuzz.Fuzzer({
+  fullName: Fuzz.string(),
+  jobTitle: Fuzz.string(),
+});
+
+var resumeFuzzer = Fuzz.Fuzzer({
+  id: Fuzz.string(),
+  userId: Fuzz.string(),
+  content: contentFuzzer(),
+});
+
+describe('Resume type', () => {
+  // based on this https://www.brianthicks.com/post/2017/04/24/add-safety-to-your-elm-json-encoders-with-fuzz-testing/
+  Fuzz.test('serialization', resumeFuzzer(), (data: Resume) => {
+    const encodedResume = Resume.encode(data);
+    const decodedResume = Resume.decode(encodedResume);
+    expect(decodedResume).toEqual(data);
   });
 });
