0b746f023fda2a09c0d9da92c4d8cc5333550747
haderman <hadermania@gmail.com>
2023-09-11 11:01:05 -0500

    Rename import for Experience component in content.stories.tsx

--- stories/editor/content/experience.stories.tsx
+++ stories/editor/content/experience.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Experience } from '@/components/editor/resume/content';
+import { ExperienceComponent as Experience } from '@/components/editor/resume/content';
 
 import { Container } from '../../helpers/container';
 
